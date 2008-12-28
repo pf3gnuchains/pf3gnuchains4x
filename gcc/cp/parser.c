002c69d3ebd042cb7f1894214d888831c6a9b0f5
monabuilder <monabuilder@dabda00f-5829-0410-a8ea-e5f24da5a5ee>
2008-12-28 03:30:50 +0000

    r125@cf-ppc-macosx:  monabuilder | 2008-12-23 19:25:51 +0900  Smerge with //gcc.gnu.org/trunk
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/pf3gnuchains/trunk@15 dabda00f-5829-0410-a8ea-e5f24da5a5ee

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -5953,6 +5953,7 @@ cp_parser_token_starts_cast_expression (cp_token *token)
     case CPP_XOR:
     case CPP_OR:
     case CPP_OR_OR:
+    case CPP_EOF:
       return false;
 
       /* '[' may start a primary-expression in obj-c++.  */
@@ -21159,7 +21160,7 @@ cp_parser_omp_for_loop (cp_parser *parser, tree clauses, tree *par_clauses)
 						    &is_direct_init,
 						    &is_non_constant_init);
 
-		      if (auto_node && !type_dependent_expression_p (init))
+		      if (auto_node && describable_type (init))
 			{
 			  TREE_TYPE (decl)
 			    = do_auto_deduction (TREE_TYPE (decl), init,
